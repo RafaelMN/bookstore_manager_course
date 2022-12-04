e8d425f1c319ff7a6899582bff6b58aae59cbd05
macedo <macedonunesrafael@gmail.com>
2022-12-04 20:40:04 -0300

    Developed project entities

---
+++ src/main/java/com/rafaelmacedo/bookstoremanager/entity/Author.java
@@ -0,0 +1,26 @@
+package com.rafaelmacedo.bookstoremanager.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false, unique = true)
+  private String name;
+  @Column(nullable = false)
+  private Integer age;
+}
